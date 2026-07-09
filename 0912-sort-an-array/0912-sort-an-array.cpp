8c91519496969a49d7f8162ff78c2f1bdd9732ff
Rakesh D <160116250+rakesh2OO5@users.noreply.github.com>
2026-07-09 11:59:40 +0530

    Time: 707 ms (13.28%), Space: 295.5 MB (32.04%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.cpp
+++ 0912-sort-an-array/0912-sort-an-array.cpp
@@ -1,50 +1,36 @@
 class Solution {
 public:
-    int partition(vector<int>& nums, int low, int high) {
-        int randomIndex = low + rand() % (high - low + 1);
-        swap(nums[randomIndex], nums[high]);
-
-        int pivot = nums[high];
-        int i = low - 1;
-
-        for (int j = low; j < high; j++) {
-            if (nums[j] <= pivot) {
-                i++;
-                swap(nums[i], nums[j]);
+    void merge(vector<int>& nums, int left, int mid, int right) {
+        vector<int> temp;
+        int i = left;
+        int j = mid + 1;
+        while (i <= mid && j <= right) {
+            if (nums[i] <= nums[j]) {
+                temp.push_back(nums[i++]);
+            } else {
+                temp.push_back(nums[j++]);
             }
         }
-
-        swap(nums[i + 1], nums[high]);
-        return i + 1;
-    }
-
-    void quickSort(vector<int>& nums, int low, int high) {
-    if (low >= high)
-        return;
-
-    int pivot = nums[low + rand() % (high - low + 1)];
-
-    int i = low;
-    int j = high;
-
-    while (i <= j) {
-        while (nums[i] < pivot) i++;
-        while (nums[j] > pivot) j--;
-
-        if (i <= j) {
-            swap(nums[i], nums[j]);
-            i++;
-            j--;
+        while (i <= mid) {
+            temp.push_back(nums[i++]);
+        }
+        while (j <= right) {
+            temp.push_back(nums[j++]);
+        }
+        for (int k = 0; k < temp.size(); k++) {
+            nums[left + k] = temp[k];
         }
     }
-
-    quickSort(nums, low, j);
-    quickSort(nums, i, high);
-}
-
+    void mergeSort(vector<int>& nums, int left, int right) {
+        if (left >= right)
+            return;
+        int mid = left + (right - left) / 2;
+        mergeSort(nums, left, mid);
+        mergeSort(nums, mid + 1, right);
+        merge(nums, left, mid, right);
+    }
     vector<int> sortArray(vector<int>& nums) {
-        srand(time(0));
-        quickSort(nums, 0, nums.size() - 1);
+        mergeSort(nums, 0, nums.size() - 1);
         return nums;
     }
 };
