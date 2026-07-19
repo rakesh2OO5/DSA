b10472151b911562f96e6f4ffadd3dd1e3e49ebe
Rakesh D <160116250+rakesh2OO5@users.noreply.github.com>
2026-07-19 15:37:24 +0530

    Time: 1 ms (37.13%), Space: 10.6 MB (57.02%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -1,23 +1,10 @@
 class Solution {
 public:
-
-    int counter(int n){
-        int count = 0;
-        while(n>0){
-            if((n&1)==1){
-                count++;
-            }
-            n=n>>1;
-        }
-        return count;
-    }
-
     vector<int> countBits(int n) {
         vector<int> res(n+1);
-        for(int i=0;i<=n;i++){
-            res[i] = counter(i);
+        for(int i=1;i<=n;i++){
+            res[i] = res[i>>1] + (i&1);
         }
-
         return res;
     }
 };
