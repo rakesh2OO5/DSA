698959fcb238a40071f349e53a8a5f326d0e3d17
Rakesh D <160116250+rakesh2OO5@users.noreply.github.com>
2026-07-22 18:35:46 +0530

    Time: 0 ms (100%), Space: 8.5 MB (86.91%) - LeetHub

---
+++ 3438-find-valid-pair-of-adjacent-digits-in-string/3438-find-valid-pair-of-adjacent-digits-in-string.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string findValidPair(string s) {
+        vector<int> freq(10, 0);
+        for (char c : s) {
+            freq[c - '0']++;
+        }
+        for (int i = 0; i < s.size() - 1; i++) {
+            int d1 = s[i] - '0';
+            int d2 = s[i + 1] - '0';
+            if (d1 != d2 && freq[d1] == d1 && freq[d2] == d2) {
+                return s.substr(i, 2);
+            }
+        }
+        return "";
+    }
+};
