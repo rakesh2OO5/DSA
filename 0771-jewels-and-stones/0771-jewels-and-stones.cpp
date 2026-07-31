06851713417c2276a69e924d47522e0762591655
Rakesh D <160116250+rakesh2OO5@users.noreply.github.com>
2026-07-31 19:17:43 +0530

    Time: 0 ms (100%), Space: 8.8 MB (25.9%) - LeetHub

---
+++ 0771-jewels-and-stones/0771-jewels-and-stones.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int numJewelsInStones(string jewels, string stones) {
+        int count = 0;
+        unordered_map<char,int> mp;
+        for(char c:stones){
+            mp[c]++;
+        }
+        for(char c:jewels){
+            if(stones.find(c)!=string::npos){
+                count+=mp[c];
+            }
+        }
+        return count;
+    }
+};
