574910738ee9ea584b919d7b0fd113b581c23f81
Rakesh D <160116250+rakesh2OO5@users.noreply.github.com>
2026-08-01 12:51:40 +0530

    Time: 0 ms (100%), Space: 9.7 MB (69.71%) - LeetHub

---
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool predictTheWinner(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> dp(nums);
+
+        for (int i = n - 2; i >= 0; i--) {
+            for (int j = i + 1; j < n; j++) {
+                dp[j] = max(
+                    nums[i] - dp[j],
+                    nums[j] - dp[j - 1]
+                );
+            }
+        }
+
+        return dp[n - 1] >= 0;
+    }
+};
