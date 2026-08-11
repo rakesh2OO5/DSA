b06954954809d0c1e3aa5ab159d0ed0d90688190
Rakesh D <160116250+rakesh2OO5@users.noreply.github.com>
2026-08-11 09:10:20 +0530

    Time: 0 ms (100%), Space: 12.9 MB (98.9%) - LeetHub

--- 3084-count-substrings-starting-and-ending-with-given-character/3084-count-substrings-starting-and-ending-with-given-character.cpp
+++ 3084-count-substrings-starting-and-ending-with-given-character/3084-count-substrings-starting-and-ending-with-given-character.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     long long countSubstrings(string s, char c) {
-        vector<int> freq(26,0);
-        for(char ch : s){
-            freq[ch-'a']++;
+        long long n = 0;
+        for(char ch : s) {
+            if(ch == c)
+                n++;
         }
-        long long n = freq[c-'a'];
-        return n * (n+1) / 2;
+        return n * (n + 1) / 2;
     }
 };
