d206a2c2bfa295576db98a610132c13b45f6234e
Rakesh D <160116250+rakesh2OO5@users.noreply.github.com>
2026-08-14 18:05:46 +0530

    Time: 0 ms (100%), Space: 7.9 MB (94.33%) - LeetHub

--- 1323-maximum-69-number/1323-maximum-69-number.cpp
+++ 1323-maximum-69-number/1323-maximum-69-number.cpp
@@ -1,24 +1,15 @@
 class Solution {
 public:
     int maximum69Number(int num) {
-        vector<int> numbers;
-        while(num > 0){
-            int digit = num % 10;
-            numbers.push_back(digit);
-            num /= 10;
-        }
-        for(int i = numbers.size() - 1; i >= 0; i--){
-            if(numbers[i] == 6){
-                numbers[i] = 9;
+        string s = to_string(num);
+
+        for(char &c : s){
+            if(c == '6'){
+                c = '9';
                 break;
             }
         }
-        int unit = 1;
-        int ans = 0;
-        for(int i = 0; i < numbers.size(); i++){
-            ans += numbers[i] * unit;
-            unit *= 10;
-        }
-        return ans;
+
+        return stoi(s);
     }
 };
