06810a87596d88c3bac727b5f7406e24a4c1f6e1
Rakesh D <160116250+rakesh2OO5@users.noreply.github.com>
2026-09-17 19:59:53 +0530

    Time: 0 ms (100%), Space: 71.7 MB (81.17%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -5,10 +5,11 @@ public:
         int maxSum = INT_MIN;
         for(int num:nums){
             currSum+=num;
-            maxSum=max(maxSum,currSum);
-            if(currSum<0)
-                currSum=0;
-        }   
+            maxSum = max(maxSum,currSum);
+            if(currSum < 0){
+                currSum = 0;
+            }
+        }
         return maxSum;
     }
 };
