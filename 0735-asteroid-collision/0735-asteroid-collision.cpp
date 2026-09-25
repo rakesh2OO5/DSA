ba4ec14bdcfd35b7d274459717484d24347fc5ca
Rakesh D <160116250+rakesh2OO5@users.noreply.github.com>
2026-09-25 12:14:03 +0530

    Time: 2 ms (32.84%), Space: 21.7 MB (69.92%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.cpp
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -3,21 +3,21 @@ public:
     vector<int> asteroidCollision(vector<int>& asteroids) {
         vector<int> result;
 
-        for (int num : asteroids) {
+        for(int num:asteroids){
             bool alive = true;
-            while (alive && num < 0 && !result.empty() && result.back() > 0) {
-                if (result.back() < abs(num)) {
+            while(alive  && !result.empty() && num <0 && result.back() >0){
+                if(result.back() < abs(num)){
                     result.pop_back();
                 }
-                else if (result.back() == abs(num)) {
+                else if(result.back() == abs(num)){
                     result.pop_back();
                     alive = false;
                 }
-                else {
+                else{
                     alive = false;
                 }
             }
-            if (alive) {
+            if(alive){
                 result.push_back(num);
             }
         }
